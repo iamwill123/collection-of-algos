e9c8f9e45abc847b64d39882dd18006e7a2632a4
iamwill123 <williampeiyuan@gmail.com>
2023-04-18 11:55:34 -0400

    some explaination to bubble

--- src/lib/sorts/bubble/index.ts
+++ src/lib/sorts/bubble/index.ts
@@ -1,3 +1,15 @@
+/*
+bubble sort is a simple sorting algorithm that repeatedly steps through the list to be sorted, compares each pair of adjacent items and swaps them if they are in the wrong order. The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted.
+
+The time complexity of bubble sort is O(n^2) in the worst case and O(n) in the best case.
+The space complexity of bubble sort is O(1).
+
+- This function sorts an array of numbers.
+- The parameter arr is the array to sort.
+- The parameter n is the number of elements in the array.
+- The function returns the sorted array.
+*/
+
 function bubble(arr: number[] = [], n: number = arr.length): number[] {
   if (n <= 1) return arr;
   for (let i = 0; i < n; i++) {
@@ -11,4 +23,4 @@ function bubble(arr: number[] = [], n: number = arr.length): number[] {
   return arr;
 }
 
-export default bubble
+export default bubble;
