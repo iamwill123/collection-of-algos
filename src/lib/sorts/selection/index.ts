d38a0c03daaf62cd5971b958e22156d1de61159c
iamwill123 <williampeiyuan@gmail.com>
2023-04-27 18:48:39 -0400

    added selection sort

---
+++ src/lib/sorts/selection/index.ts
@@ -0,0 +1,45 @@
+/*
+Selection sort, find the smallest element in the array, then swap with the first element
+*/
+
+import {
+	endTime,
+	howLongExecTook,
+	isAnObj,
+	isAsc,
+	startTime,
+} from '../../helpers'
+import { SortInput, SortOutput } from '../../../types/sorts'
+
+function selection(input: SortInput): SortOutput {
+	const _s = startTime()
+	const { arr, order = 'asc', key = '' } = input
+
+	const n: number = arr.length
+
+	for (let k = 0; k < n - 1; k++) {
+		// initialize min to k, the first element
+		let min = k
+		for (let i = k + 1; i < n; i++) {
+			// find where the smallest element is and grab it's index
+			const leftNum = isAsc(order) ? i : min
+			const rightNum = isAsc(order) ? min : i
+
+			let _leftNum = isAnObj(leftNum, arr) ? arr[leftNum][key] : arr[leftNum]
+			let _rightNum = isAnObj(rightNum, arr)
+				? arr[rightNum][key]
+				: arr[rightNum]
+
+			if (_leftNum < _rightNum) {
+				min = i
+			}
+		}
+
+		;[arr[k], arr[min]] = [arr[min], arr[k]]
+	}
+	const _e = endTime()
+	const execTimeInMs = howLongExecTook(_s, _e)
+	return { arr, key, order, n, execTime: execTimeInMs }
+}
+
+export default selection
---
+++ test/sorts/selection/index.test.ts
@@ -0,0 +1,56 @@
+import selection from '../../../src/lib/sorts/selection'
+import { cases } from '../test-cases'
+
+describe('selection sort', () => {
+	test(cases[1], () => {
+		const obj = { arr: [5, 3, 1, 4, 2] }
+		const { arr: arrOfNums } = selection(obj)
+		expect(arrOfNums).toEqual([1, 2, 3, 4, 5])
+	})
+
+	test(cases[2], () => {
+		const obj = { arr: [5, 3, 1, 4, 2], order: 'desc' }
+
+		const { arr: arrOfNums } = selection(obj)
+		expect(arrOfNums).toEqual([5, 4, 3, 2, 1])
+	})
+
+	test(cases[3], () => {
+		const obj = {
+			arr: [
+				{ name: 'John', age: 23 },
+				{ name: 'Mike', age: 32 },
+				{ name: 'Chris', age: 11 },
+				{ name: 'Nick', age: 19 },
+			],
+			key: 'age',
+		}
+		const { arr: arrOfObjs } = selection(obj)
+
+		expect(arrOfObjs).toEqual([
+			{ name: 'Chris', age: 11 },
+			{ name: 'Nick', age: 19 },
+			{ name: 'John', age: 23 },
+			{ name: 'Mike', age: 32 },
+		])
+	})
+	test(cases[4], () => {
+		const obj = {
+			arr: [
+				{ name: 'John', height: 23 },
+				{ name: 'Mike', height: 32 },
+				{ name: 'Chris', height: 11 },
+				{ name: 'Nick', height: 19 },
+			],
+			key: 'height',
+			order: 'desc',
+		}
+		const { arr: arrOfObjs } = selection(obj)
+		expect(arrOfObjs).toEqual([
+			{ name: 'Mike', height: 32 },
+			{ name: 'John', height: 23 },
+			{ name: 'Nick', height: 19 },
+			{ name: 'Chris', height: 11 },
+		])
+	})
+})
