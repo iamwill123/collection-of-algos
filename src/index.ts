8a7dc09f1ca5d43a768a19da3b09329bdf93938d
iamwill123 <williampeiyuan@gmail.com>
2023-04-27 20:36:42 -0400

    export selection sort

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import bubbleSort from './lib/sorts/bubble'
 import nativeSort from './lib/sorts/native'
+import selectionSort from './lib/sorts/selection'
 
-export { nativeSort, bubbleSort }
+export { nativeSort, bubbleSort, selectionSort }
