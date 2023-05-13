f6b329b017b4895ddd3044fd2895ac9aced6a269
iamwill123 <williampeiyuan@gmail.com>
2023-05-13 12:15:27 -0400

    add more code coverage

--- test/sorts/selection/index.test.ts
+++ test/sorts/selection/index.test.ts
@@ -49,4 +49,37 @@ describe('Selection sort', () => {
 		const { arr: arrOfObjs } = await selection(obj)
 		expect(arrOfObjs).toEqual(sorted({ key, order }).arr.objects)
 	})
+
+	test('cases[5]: ' + cases[5], async () => {
+		const obj = {
+			arr: unsorted().arr.numbers,
+			isSorting: () => false,
+		}
+		const { arr: arrOfNums } = await selection(obj)
+		expect(arrOfNums).toEqual(unsorted().arr.numbers)
+	})
+
+	test('cases[6]: ' + cases[6], async () => {
+		const obj = {
+			arr: [5, 7, 9, 55, 2, 66],
+			callback: async (a: number, b: number) => {
+				return await Promise.resolve()
+			},
+		}
+		const { arr: arrOfNums, animate } = await selection(obj)
+		expect(arrOfNums).toEqual([2, 5, 7, 9, 55, 66])
+		expect(animate).toEqual(true)
+	})
+
+	test('cases[7]: ' + cases[7], async () => {
+		const obj = {
+			arr: [5, 7, 9, 55, 2, 66],
+			callback: async () => {
+				return await Promise.resolve()
+			},
+		}
+		const { arr: arrOfNums, animate } = await selection(obj)
+		expect(arrOfNums).toEqual([2, 5, 7, 9, 55, 66])
+		expect(animate).toEqual(false)
+	})
 })
--- test/sorts/test-cases.ts
+++ test/sorts/test-cases.ts
@@ -4,6 +4,9 @@ const cases = {
 	2: 'should sort an array of numbers in descending order when provided a value of "desc" to the order prop',
 	3: 'should sort an array of objects in ascending order (by default) when provided an arr key with an [] of {}s, and a key prop',
 	4: 'should sort an array of objects in descending order when provided an arr key with an [] of {}s, a key prop, and value of "desc" to the order prop',
+	5: 'should return the original array if isSorting is false',
+	6: 'should call the callback with the current array and set animation to true',
+	7: 'should not call the callback if not argument is provided and animation is false',
 }
 
 export { cases }
