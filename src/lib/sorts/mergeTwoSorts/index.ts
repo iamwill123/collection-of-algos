ed0f3f11447865f4c0352fdf23ba937f42000039
iamwill123 <williampeiyuan@gmail.com>
2023-07-01 12:01:06 -0400

    complete unit tests and fix and issues

--- src/lib/sorts/mergeTwoSorts/index.ts
+++ src/lib/sorts/mergeTwoSorts/index.ts
@@ -1,13 +1,11 @@
-import {
-  endTime,
-  howLongExecTook,
-  isAnObj,
-  isAsc,
-  startTime,
-} from '../../helpers';
+// This function merges two sorted arrays into one sorted array in O(n) time and O(n) space complexity (where firstLen is the length of the first array) by using three pointers (one for first, two for second array) and a main pointer (for the second array) to keep track of the current index of the second array. The first pointer starts at the end of the first array, the second pointer starts at the end of the second array, and the main pointer starts at the end of the second array (after adding in firstLen of 0s to second array). The first pointer is used to compare with the second pointer, and the larger of the two is placed at the main pointer index. The main pointer is then decremented. This process is repeated until the first pointer is less than 0. If there are remaining elements in the first array, they are moved to the second array.
+
+import { endTime, howLongExecTook, isAsc, startTime } from '../../helpers';
 import { MergeTwoSortsInput, SortOutput } from '../../../types/sorts';
 
-function merge_first_sorted_into_second_sorted(input: MergeTwoSortsInput): SortOutput {
+function merge_first_sorted_into_second_sorted(
+  input: MergeTwoSortsInput,
+): SortOutput {
   const _s = startTime();
   const { first = [], second = [], order = 'asc', key = '' } = input;
 
@@ -18,7 +16,7 @@ function merge_first_sorted_into_second_sorted(input: MergeTwoSortsInput): SortO
     secondArray = (second as any[]).concat(
       key.length && typeof key === 'string'
         ? Array.from({ length: firstLen }, () => ({ [key]: 0 }))
-        : Array.from({ length: firstLen }, () => 0)
+        : Array.from({ length: firstLen }, () => 0),
     ),
     mainPointer = secondArray.length - 1; // start at the end of the secondArray
 
@@ -31,7 +29,7 @@ function merge_first_sorted_into_second_sorted(input: MergeTwoSortsInput): SortO
     let condition = key
       ? swapCondition(
           firstArray[firstPointer][key],
-          secondArray[secondPointer][key]
+          secondArray[secondPointer][key],
         )
       : swapCondition(firstArray[firstPointer], secondArray[secondPointer]);
 
--- test/sorts/mergeTwoSorts/index.test.ts
+++ test/sorts/mergeTwoSorts/index.test.ts
@@ -1,71 +1,164 @@
 import mergeTwoSorts from '../../../src/lib/sorts/mergeTwoSorts';
-import { cases } from '../test-cases';
-import { testData } from '../test-data';
 
 describe('mergeTwoSorts sort', () => {
-  const { unsorted, sorted } = testData;
-
-  test('cases[0]: given empty first array, it returns an empty []', () => {
+  test('cases[0]: given empty first list, it returns an empty list', () => {
     const obj1 = { first: [] };
     const { arr: arrOfNums1 } = mergeTwoSorts(obj1);
     expect(arrOfNums1).toEqual([]);
   });
 
-  test('cases[1]: given a ASC sorted first and second array, it returns an ASC ordered merged list', () => {
+  test('cases[1]: given both ASC order sorted first and second list, it returns an ASC ordered merged list', () => {
     const obj = { first: [2, 3, 5], second: [1, 4, 6] };
     const { arr: arrOfNums } = mergeTwoSorts(obj);
-    console.log('arrOfNums', arrOfNums);
     expect(arrOfNums).toEqual([1, 2, 3, 4, 5, 6]);
+
+    const obj1 = { first: [2, 3], second: [1, 4, 6, 10, 22] };
+    const { arr: arrOfNums1 } = mergeTwoSorts(obj1);
+    expect(arrOfNums1).toEqual([1, 2, 3, 4, 6, 10, 22]);
   });
 
-  test('cases[2]: given a DESC sorted first and seconds array, it returns an DESC ordered merged list', () => {
+  test('cases[2]: given both DESC order sorted first and second list, it returns a DESC ordered merged list', () => {
     const obj = { first: [5, 3, 2], second: [6, 4, 1], order: 'desc' };
     const { arr: arrOfNums } = mergeTwoSorts(obj);
-    console.log('arrOfNums', arrOfNums);
     expect(arrOfNums).toEqual([6, 5, 4, 3, 2, 1]);
+
+    const obj1 = { first: [5, 3, 2, 1], second: [10, 4, 0], order: 'desc' };
+    const { arr: arrOfNums1 } = mergeTwoSorts(obj1);
+    expect(arrOfNums1).toEqual([10, 5, 4, 3, 2, 1, 0]);
   });
 
-  // test('cases[1]: ' + cases[1], () => {
-  //   const obj = { arr: unsorted().arr.numbers };
-  //   const { arr: arrOfNums } = mergeTwoSorts(obj);
-  //   expect(arrOfNums).toEqual(sorted().arr.numbers);
-  // });
+  test('cases[3]: given both ASC order sorted first and second list of objects, it returns an ASC ordered merged list of objects (also handles floats, negative nums, alphabet chars)', () => {
+    const obj = {
+      first: [{ value: 0 }, { value: 3 }, { value: 5 }],
+      second: [{ value: 1 }, { value: 4 }, { value: 6 }],
+      key: 'value',
+    };
+    const { arr: arrOfNums } = mergeTwoSorts(obj);
+    expect(arrOfNums).toEqual([
+      { value: 0 },
+      { value: 1 },
+      { value: 3 },
+      { value: 4 },
+      { value: 5 },
+      { value: 6 },
+    ]);
 
-  // test('cases[2]: ' + cases[2], () => {
-  //   let order = 'desc';
-  //   const obj = { arr: unsorted().arr.numbers, order };
-  //   const { arr: arrOfNums } = mergeTwoSorts(obj);
-  //   expect(arrOfNums).toEqual(sorted({ order }).arr.numbers);
-  // });
+    // handle negative numbers
+    const obj1 = {
+      first: [{ value: -2 }, { value: -1 }, { value: 5 }],
+      second: [{ value: -99 }, { value: -10 }, { value: 6 }],
+      key: 'value',
+    };
+    const { arr: arrOfNums1 } = mergeTwoSorts(obj1);
+    expect(arrOfNums1).toEqual([
+      { value: -99 },
+      { value: -10 },
+      { value: -2 },
+      { value: -1 },
+      { value: 5 },
+      { value: 6 },
+    ]);
 
-  // test('cases[3]: ' + cases[3], () => {
-  //   const obj = {
-  //     arr: unsorted().arr.objects,
-  //     key: 'age',
-  //   };
-  //   const { arr: arrOfObjs } = mergeTwoSorts(obj);
+    // handle floats
+    const obj2 = {
+      first: [{ value: -2.2 }, { value: -1.1 }, { value: 5.5 }],
+      second: [{ value: -99.9 }, { value: -10.1 }, { value: 6.6 }],
+      key: 'value',
+    };
+    const { arr: arrOfNums2 } = mergeTwoSorts(obj2);
 
-  //   expect(arrOfObjs).toEqual(sorted({}).arr.objects);
-  // });
+    expect(arrOfNums2).toEqual([
+      { value: -99.9 },
+      { value: -10.1 },
+      { value: -2.2 },
+      { value: -1.1 },
+      { value: 5.5 },
+      { value: 6.6 },
+    ]);
 
-  // test('cases[4]: ' + cases[4], () => {
-  //   let key = 'height',
-  //     order = 'desc';
-  //   const obj = {
-  //     arr: unsorted({ key }).arr.objects,
-  //     key,
-  //     order,
-  //   };
-  //   const { arr: arrOfObjs } = mergeTwoSorts(obj);
-  //   expect(arrOfObjs).toEqual(sorted({ key, order }).arr.objects);
-  // });
+    // handle alphabet chars
+    const obj3 = {
+      first: [{ value: 'a' }, { value: 'b' }, { value: 'c' }],
+      second: [{ value: 'd' }, { value: 'e' }, { value: 'f' }],
+      key: 'value',
+    };
+    const { arr: arrOfNums3 } = mergeTwoSorts(obj3);
+    expect(arrOfNums3).toEqual([
+      { value: 'a' },
+      { value: 'b' },
+      { value: 'c' },
+      { value: 'd' },
+      { value: 'e' },
+      { value: 'f' },
+    ]);
+  });
 
-  // test('cases[5]: ' + cases[5], () => {
-  //   const obj = {
-  //     arr: unsorted().arr.numbers,
-  //     isSorting: () => false,
-  //   };
-  //   const { arr: arrOfNums } = mergeTwoSorts(obj);
-  //   expect(arrOfNums).toEqual(unsorted().arr.numbers);
-  // });
+  test('cases[4]: given both DESC order sorted first and second list of objects, it returns an DESC ordered merged list of objects (also handles floats, negative nums, alphabet chars)', () => {
+    const obj = {
+      first: [{ value: 5 }, { value: 3 }, { value: 2 }],
+      second: [{ value: 6 }, { value: 4 }, { value: 1 }],
+      key: 'value',
+      order: 'desc',
+    };
+    const { arr: arrOfNums } = mergeTwoSorts(obj);
+    expect(arrOfNums).toEqual([
+      { value: 6 },
+      { value: 5 },
+      { value: 4 },
+      { value: 3 },
+      { value: 2 },
+      { value: 1 },
+    ]);
+
+    // handle negative numbers
+    const obj1 = {
+      first: [{ value: 5 }, { value: 3 }, { value: -2 }],
+      second: [{ value: 6 }, { value: 1 }, { value: -4 }],
+      key: 'value',
+      order: 'desc',
+    };
+    const { arr: arrOfNums1 } = mergeTwoSorts(obj1);
+    expect(arrOfNums1).toEqual([
+      { value: 6 },
+      { value: 5 },
+      { value: 3 },
+      { value: 1 },
+      { value: -2 },
+      { value: -4 },
+    ]);
+
+    // handle floats
+    const obj2 = {
+      first: [{ value: 5.5 }, { value: 3.3 }, { value: -2.2 }],
+      second: [{ value: 6.6 }, { value: 1.1 }, { value: -4.4 }],
+      key: 'value',
+      order: 'desc',
+    };
+    const { arr: arrOfNums2 } = mergeTwoSorts(obj2);
+    expect(arrOfNums2).toEqual([
+      { value: 6.6 },
+      { value: 5.5 },
+      { value: 3.3 },
+      { value: 1.1 },
+      { value: -2.2 },
+      { value: -4.4 },
+    ]);
+
+    // handle alphabet chars
+    const obj3 = {
+      first: [{ value: 'c' }, { value: 'b' }, { value: 'a' }],
+      second: [{ value: 'f' }, { value: 'e' }, { value: 'd' }],
+      key: 'value',
+      order: 'desc',
+    };
+    const { arr: arrOfNums3 } = mergeTwoSorts(obj3);
+    expect(arrOfNums3).toEqual([
+      { value: 'f' },
+      { value: 'e' },
+      { value: 'd' },
+      { value: 'c' },
+      { value: 'b' },
+      { value: 'a' },
+    ]);
+  });
 });
